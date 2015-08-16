e042439da010573ef92cca10c5cc2586fc7df878
Carles Tubio <ctubio@users.noreply.github.com>
2015-08-16 06:43:15 +0200

    Added beep if heat increases.

--- cgminer-fan.sh
+++ cgminer-fan.sh
@@ -28,9 +28,16 @@ while true; do
         test $pwm -ne $FAN_MIN && FAN_MOD=$FAN_MIN;
       fi;
       if [ $FAN_MOD -lt 0 ]; then
+        echo 1 > /sys/class/gpio/gpio20/value && \
+        sleep 3 && \
+        echo 0 > /sys/class/gpio/gpio20/value;
         echo -n "DIE at " && date && \
         /etc/init.d/cgminer.sh stop > /dev/null 2>&1;
       elif [ $FAN_MOD -gt 0 ]; then
+        test $pwm -lt $FAN_MOD && \
+        echo 1 > /sys/class/gpio/gpio20/value && \
+        sleep 1 && \
+        echo 0 > /sys/class/gpio/gpio20/value;
         sed -i "/bitmain-fan-pwm/c\"bitmain-fan-pwm\" : \"$FAN_MOD\"," /config/cgminer.conf  && \
         echo -n "SET $FAN_MOD% at " && date && \
         /etc/init.d/cgminer.sh restart > /dev/null 2>&1;
